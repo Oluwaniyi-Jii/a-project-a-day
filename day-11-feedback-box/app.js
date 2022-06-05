af1cab505f49a62b92347b45e879bb73b450a785
Oluwaniyi-Jii <79464168+Oluwaniyi-Jii@users.noreply.github.com>
2022-06-05 18:29:47 +0100

    added comments

--- day-11-feedback-box/app.js
+++ day-11-feedback-box/app.js
@@ -4,7 +4,15 @@ const sendBtn = document.querySelector('#send')
 const panel = document.querySelector('#panel')
 let selectedRating = 'Satisfied'
 
+/* Adding an event listener to the ratingsContainer. */
 ratingsContainer.addEventListener('click', (e) => {
+    /* Checking if the parentNode of the element clicked has a class of rating and if the
+    nextElementSibling exists. If it does, it removes the active class from all the ratings and adds
+    the active class to the parentNode of the element clicked. It also sets the selectedRating to
+    the nextElementSibling's innerHTML. If the parentNode of the element clicked has a class of
+    rating and the previousSibling exists and the previousElementSibling's nodeName is an image, it
+    removes the active class from all the ratings and adds the active class to the parentNode of the
+    element clicked. It also sets the selectedRating to the innerHTML of the element clicked. */
     if (e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
         removeActive()
         e.target.parentNode.classList.add('active')
